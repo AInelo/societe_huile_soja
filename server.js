3d33465f9debf770754d757fc902dc99aaa8c6a1
TOTON Lionel <totonlionel@gmail.com>
2024-02-02 21:38:06 +0100

    La société d'Huile

--- server.js
+++ server.js
@@ -24,37 +24,25 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-// const start = async () => {
-//   try {
-//     await connectDB(process.env.MONGO_URI);
-//     app.listen(port, () =>
-//       console.log(`Server is listening on port ${port}...`)
-//     );
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+//  // Obtenez l'adresse IP de la première interface réseau non interne
+//  const networkInterfaces = os.networkInterfaces();
+//  // console.log(networkInterfaces);
+//  const ipAddress = Object.values(networkInterfaces)
+//    .flat()
+//    .filter((iface) => iface.family === 'IPv4' && !iface.internal)[0].address;
 
+//  // console.log(ipAddress);
 
- // Obtenez l'adresse IP de la première interface réseau non interne
- const networkInterfaces = os.networkInterfaces();
- // console.log(networkInterfaces);
- const ipAddress = Object.values(networkInterfaces)
-   .flat()
-   .filter((iface) => iface.family === 'IPv4' && !iface.internal)[0].address;
+//  const Port = process.env.PORT || 3000;
 
- // console.log(ipAddress);
-
- const Port = process.env.PORT || 3000;
-
- app.listen(Port, ipAddress, () => {
-   console.log(`Le serveur écoute sur ${ipAddress}:${Port}.....`);
- });
+//  app.listen(Port, ipAddress, () => {
+//    console.log(`Le serveur écoute sur ${ipAddress}:${Port}.....`);
+//  });
 
 
       
-// app.listen(port, () =>
-//     console.log(`Server is listening on port ${port}...`)
-// );
+app.listen(port, () =>
+    console.log(`Server is listening on port ${port}...`)
+);
 
 
