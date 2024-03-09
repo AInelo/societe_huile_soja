21dfd8ba67755e5e20f55f1e69fe4c6cb25479a7
TOTON Lionel <totonlionel@gmail.com>
2024-03-09 15:18:40 +0100

    Finalisation

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-
 const os = require('os');
 const path = require('path');
 // const member = require('./routes/member');
@@ -24,25 +23,24 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-// // Obtenez l'adresse IP de la première interface réseau non interne
-// const networkInterfaces = os.networkInterfaces();
-// // console.log(networkInterfaces);
-// const ipAddress = Object.values(networkInterfaces)
-//     .flat()
-//     .filter((iface) => iface.family === 'IPv4' && !iface.internal)[0].address;
+// Obtenez l'adresse IP de la première interface réseau non interne
+const networkInterfaces = os.networkInterfaces();
+// console.log(networkInterfaces);
+const ipAddress = Object.values(networkInterfaces)
+    .flat()
+    .filter((iface) => iface.family === 'IPv4' && !iface.internal)[0].address;
 
-// // console.log(ipAddress);
+// console.log(ipAddress);
 
-// const Port = process.env.PORT || 3000;
-
-// app.listen(Port, ipAddress, () => {
-//     console.log(`Le serveur écoute sur ${ipAddress}:${Port}.....`);
-// });
-
+const Port = process.env.PORT || 3000;
 
 
 app.listen(port, () =>
     console.log(`Server is listening on port ${port}...`)
 );
+
+app.listen(Port, ipAddress, () => {
+    console.log(`Le serveur écoute sur ${ipAddress}:${Port}.....`);
+});
 
 
