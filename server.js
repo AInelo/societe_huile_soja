b1e70c3c4d53b5a36cfe6e6bc23a70dbd6825b5d
TOTON Lionel <totonlionel@gmail.com>
2024-03-09 15:45:47 +0100

    La société d'Huile

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const os = require('os');
-const path = require('path');
+const os = require("os");
+const path = require("path");
 // const member = require('./routes/member');
 // const connectDB = require('./db/connectDB');
-require('dotenv').config();
-const notFound = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
-
 
 // routes
 // app.use('/api/v1/member', member);
@@ -20,27 +19,21 @@ app.use(express.json());
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-
 const port = process.env.PORT || 5000;
 
 // Obtenez l'adresse IP de la première interface réseau non interne
 const networkInterfaces = os.networkInterfaces();
 // console.log(networkInterfaces);
 const ipAddress = Object.values(networkInterfaces)
-    .flat()
-    .filter((iface) => iface.family === 'IPv4' && !iface.internal)[0].address;
+  .flat()
+  .filter((iface) => iface.family === "IPv4" && !iface.internal)[0].address;
 
 // console.log(ipAddress);
 
 const Port = process.env.PORT || 3000;
 
-
-app.listen(port, () =>
-    console.log(`Server is listening on port ${port}...`)
-);
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 
 app.listen(Port, ipAddress, () => {
-    console.log(`Le serveur écoute sur ${ipAddress}:${Port}.....`);
+  console.log(`Le serveur écoute sur ${ipAddress}:${Port}.....`);
 });
-
-
